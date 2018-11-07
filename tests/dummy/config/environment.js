f69e46ddeb1634f7497f2d3f4c5ac4455f183cbc
Kit Sunde <kitsunde@gmail.com>
2018-11-08 00:49:12 +0800

    Fix the demo path

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -45,7 +45,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     ENV.locationType = 'hash';
-    ENV.rootURL = '/@mediapop/ember-viewport/';
+    ENV.rootURL = '/ember-viewport/';
     // here you can enable a production-specific feature
   }
 
